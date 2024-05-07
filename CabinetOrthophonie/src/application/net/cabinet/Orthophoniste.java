d3036f6a32c57785a0929adfd720a4d4aa11cb6a
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-05-08 00:39:53 +0100

    Update Orthophoniste.java

--- CabinetOrthophonie/src/application/net/cabinet/Orthophoniste.java
+++ CabinetOrthophonie/src/application/net/cabinet/Orthophoniste.java
@@ -1,10 +1,34 @@
 package application.net.cabinet;
 
+import java.util.Map;
+
 public class Orthophoniste {
 	
-	Compte compte;
-	Agenda agenda;
+	private Compte compte;
+	private Agenda agenda;
+	private Map<Integer,DossierPatient> dossiers;
+	private Map<Integer,Patient> patients;
 	
+	
+	
+	public Orthophoniste(Compte compte, Agenda agenda,
+			Map<Integer, DossierPatient> dossiers,
+			Map<Integer, Patient> patients) {
+		super();
+		this.compte = compte;
+		this.agenda = agenda;
+		this.dossiers = dossiers;
+		this.patients = patients;
+	}
+	
+	
+
+	public Orthophoniste() {
+		super();
+	}
+
+
+
 	/*
 	 * Consultation
 	 */
@@ -28,4 +52,37 @@ public class Orthophoniste {
 			String thematique,int[] idpatients){
 		//TODO
 	}
+	
+	public Compte getCompte() {
+		return compte;
+	}
+
+	public void setCompte(Compte compte) {
+		this.compte = compte;
+	}
+
+	public Agenda getAgenda() {
+		return agenda;
+	}
+
+	public void setAgenda(Agenda agenda) {
+		this.agenda = agenda;
+	}
+
+	public Map<Integer, DossierPatient> getDossiers() {
+		return dossiers;
+	}
+
+	public void setDossiers(Map<Integer, DossierPatient> dossiers) {
+		this.dossiers = dossiers;
+	}
+
+	public Map<Integer,Patient> getPatients() {
+		return patients;
+	}
+
+	public void setPatients(Map<Integer,Patient> patients) {
+		this.patients = patients;
+	}
+
 }
