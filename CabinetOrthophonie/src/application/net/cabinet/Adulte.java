fb993e29e69568db5817b5c12deb046bb8ec6284
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-05-08 01:14:47 +0100

    Adding some encapsulation

--- CabinetOrthophonie/src/application/net/cabinet/Adulte.java
+++ CabinetOrthophonie/src/application/net/cabinet/Adulte.java
@@ -1,11 +1,41 @@
 package application.net.cabinet;
 
+import java.time.LocalDate;
+
 public class Adulte extends Patient {
 
 	private String diplome;
 	private String profession;
 	private String tel;
 	
+	
+	public Adulte(String nom, String prenom, int age,
+			LocalDate dateDeNaissance, String lieuDeNaissance,
+			boolean isFirstTime, int idDossier, String diplome,
+			String profession, String tel) {
+		super(nom, prenom, age, dateDeNaissance, lieuDeNaissance, isFirstTime,
+				idDossier);
+		this.diplome = diplome;
+		this.profession = profession;
+		this.tel = tel;
+	}
+	
+	
+	public Adulte(String nom, String prenom, int age,
+			LocalDate dateDeNaissance, String lieuDeNaissance,
+			boolean isFirstTime, int idDossier) {
+		super(nom, prenom, age, dateDeNaissance, lieuDeNaissance, isFirstTime,
+				idDossier);
+	}
+	
+	
+
+
+	public Adulte() {
+		super();
+	}
+
+
 	public String getDiplome() {
 		return diplome;
 	}
